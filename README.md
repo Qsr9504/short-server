5068b06bca91428ea3139f23c8eeab459cb1bca3
faker <faker@shatang.com>
2024-11-21 15:49:40 +0800

    编译docker文件

--- README.md
+++ README.md
@@ -20,3 +20,27 @@ curl --location --request GET 'localhost:7777/stats/0af707bd' \
     "long_url":"https://www.baidu.com/s?wd=hello"
 }'
 ```
+
+## 三、Docker 拉取命令
+```bash
+docker pull qsr9504/short-server:latest
+```
+
+docker 启动命令
+```bash
+docker run --name shortlink-server --restart=always -v ${你自己的配置文件路径}/config.yaml:/app/config.yaml -p 7777:7777 qsr9504/short-server:latest
+```
+
+配置文件模版文件
+```yaml
+base:
+  website: 'http://192.168.5.8:7777'   # 域名 指向到该服务及其端口
+  port: '7779'  # 服务启动端口
+  length: 8     # 短链接秘钥生成长度
+  cacheTime: 60 # 内存缓存时间，分钟
+
+redis:
+  addr: '192.168.5.8'   # 地址
+  port: '6379'      # 端口号
+  pwd:  'shatang'   # 密码
+```
---
+++ build.sh
@@ -0,0 +1,24 @@
+server_name=short-server
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -tags jsoniter -o mamba-short-server .
+upx -9 mamba-short-server -o short-server
+
+cat > ./Dockerfile << EOF
+FROM alpine:latest
+RUN apk update && apk add busybox-extras tzdata
+WORKDIR /app
+# 将编译好的程序拷贝进容器中
+COPY ./short-server .
+COPY ./config.yaml /app
+ENTRYPOINT ["./short-server"]
+EOF
+docker build -t ${server_name} .
+docker tag ${server_name} qsr9504/short-server:latest
+docker tag ${server_name} qsr9504/short-server:1.0.0
+docker push qsr9504/short-server:latest
+docker push qsr9504/short-server:1.0.0
+
+docker rmi qsr9504/short-server:latest
+docker rmi qsr9504/short-server:1.0.0
+docker rmi ${server_name}
+rm -f mamba-short-server
